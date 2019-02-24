6a1845b545622890ef33ef752b2fb558fb66119c
h_takamatsu <h_takamatsu@kost.jp>
2019-02-24 15:42:04 +0900

    run success storybook

---
+++ storybook/.storybook/config.js
@@ -0,0 +1,27 @@
+import { configure } from '@storybook/vue';
+
+import Vue from 'vue';
+import Vuex from 'vuex'; // Vue plugins をインポートします
+
+// Import your custom components.
+// Custom Component を import します。
+// import Mybutton from '../src/stories/Button.vue';
+import Logo from '../../app/components/Logo.vue';
+
+// Install Vue plugins.
+// Vue plugin をインストールします。
+Vue.use(Vuex);
+
+// Register custom components.
+// Custom Component を登録します。
+// Vue.component('my-button', Mybutton);
+Vue.component('Logo', Logo);
+
+function loadStories() {
+  // 必要な Story を require します。
+  // You can require as many stories as you need.
+  // require('../src/stories');
+  require('../stories');
+}
+
+configure(loadStories, module);
---
+++ storybook/stories/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+
+import { storiesOf } from '@storybook/vue';
+
+// import MyButton from './Button.vue';
+import Logo from './../../app/components/Logo.vue';
+
+storiesOf('Logo', module)
+  .add('story as a template',
+    // () => '<my-button :rounded="true">story as a function template</my-button>'
+    '<Logo />'
+  )
+  // .add('story as a component', () => ({
+  //   components: { MyButton },
+  //   template: '<my-button :rounded="true">rounded</my-button>'
+  // }));
