3fe79e2b1ebcd73beda72cb820d3b20f79c77cb0
h_takamatsu <h_takamatsu@kost.jp>
2019-03-02 22:18:26 +0900

    refactaring webpack.config

--- storybook/.storybook/webpack.config.js
+++ storybook/.storybook/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.resolve.alias['@storybook/vue$'] = path.resolve(__dirname, '../node_modules/@storybook/vue')
-  defaultConfig.resolve.alias['@'] = path.resolve(__dirname, '../../app');
-  defaultConfig.resolve.alias['~'] = path.resolve(__dirname, '../../app');
-  return defaultConfig;
+module.exports = (baseConfig, env, config) => {
+  config.resolve.alias['@storybook/vue'] = path.resolve(__dirname, '../node_modules/@storybook/vue')
+  config.resolve.alias['@'] = path.resolve(__dirname, '../../app');
+  config.resolve.alias['~'] = path.resolve(__dirname, '../../app');
+  return config;
 }
